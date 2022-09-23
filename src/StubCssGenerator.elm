373b81e3e337afdc1494fe28422d95833fbde094
Dillon Kearns <dillon@incrementalelm.com>
2022-09-23 16:21:27 -0700

    Add prototype of file to apply elm-review fix to.

---
+++ src/StubCssGenerator.elm
@@ -0,0 +1,12 @@
+module StubCssGenerator exposing (..)
+
+
+type alias GeneratedClass =
+    { name : String
+    , body : String
+    }
+
+
+generatedCssListHere : List GeneratedClass
+generatedCssListHere =
+    []
