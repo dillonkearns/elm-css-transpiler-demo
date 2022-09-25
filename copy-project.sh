3e048427588f28c95c85fc98ff7a8508f2aab103
Dillon Kearns <dillon@incrementalelm.com>
2022-09-25 13:34:11 -0700

    Run elm make from secret folder.

--- copy-project.sh
+++ copy-project.sh
@@ -4,10 +4,10 @@
 TMP_DIR=".elm-css-transpiler"
 mkdir -p $TMP_DIR
 cp -r src/ $TMP_DIR/src
-# (cd $TMP_DIR && elm-review --config ../elm-review-elm-css-extract/preview/ --fix-all-without-prompt --debug --elmjson elm.json  && cat src/StubCssGenerator.elm)
+mkdir dist
 npx node ./run.js
 cp $TMP_DIR/styles.css dist/styles.css
-elm make src/Main.elm --output dist/elm.js
+(cd $TMP_DIR && elm make src/Main.elm --output ../dist/elm.js)
 
 # - [X] Run elm-review in auto-fix mode on the secret folder -> `StubCssGenerator.styles : String`  is populated in secret folder
 # - [X] Create a `Platform.worker` sends `StubCssGenerator.styles` through a port, and write that to an Elm file that includes a Platform.worker setup (`ExtractCss.elm`)
